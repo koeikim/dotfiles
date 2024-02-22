ac5917cce91ea40596ac72db5fa183beab72fec4
Kyle Kim <kyle.kim@sonatus.com>
2024-02-22 11:32:03 +0900

    update wezterm configuration

--- .config/wezterm/.wezterm.lua
+++ .config/wezterm/.wezterm.lua
@@ -1,9 +1,27 @@
 local wezterm = require 'wezterm';
-return {
-    font = wezterm.font("JetBrainsMono Nerd Font"),
-	font_size = 11.0,
-    default_domain = "WSL:ArchLinux",
-    default_prog = { "wsl.exe" },
-    color_scheme = "OneHalfDark",
-	window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+local config = wezterm.config_builder()
+
+config.font = wezterm.font "JetBrainsMono Nerd Font"
+config.font_size = 11.0
+config.font_shaper = "Harfbuzz"
+config.default_domain = "WSL:ArchLinux"
+config.default_prog = { "wsl.exe" }
+config.color_scheme = "OneHalfDark"
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.freetype_load_target = "Normal"
+config.freetype_render_target = 'HorizontalLcd'
+config.keys = {
+	-- This will create a new split and run your default program inside it
+	{
+		key = '=',
+		mods = 'SHIFT|ALT',
+		action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+	},
+	{
+		key = '%',
+		mods = 'SHIFT|ALT',
+		action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+	},
 }
+
+return config
