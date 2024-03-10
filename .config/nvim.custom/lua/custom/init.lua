f515dedcb038a1cb34b176c14ea4f39df4a94a6e
Kyle Kim <kyle.kim@sonatus.com>
2024-03-10 17:08:30 +0000

    update nvchad 2.5 and use my repo

--- .config/nvim.custom/lua/custom/init.lua
+++
@@ -1,8 +0,0 @@
-local opt = vim.opt
-
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
-opt.expandtab = true
-opt.autoindent = true
-opt.smartindent = true
--- setup_dev.sh
+++ setup_dev.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-NVIM_VER=0.9.5
-
 mkdir -p $HOME/nvim_install
 cd $HOME/nvim_install
-wget https://github.com/neovim/neovim/releases/download/v${NVIM_VER}/nvim-linux64.tar.gz
+wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 tar xvf nvim-linux64.tar.gz
 cp -arf nvim-linux64/* $HOME/.local/
 rm -rf nvim-linux64 nvim-linux64.tar.gz
 cd ..
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
-cp -arf $HOME/.config/nvim.custom/* $HOME/.config/nvim/
 rmdir $HOME/nvim_install
+git clone https://github.com/koeikim/nvchad_config.git ~/.config/nvim --depth 1
